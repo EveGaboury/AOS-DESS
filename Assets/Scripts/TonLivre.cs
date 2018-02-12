914788a0a14a16ce5487868ecc9f3f7905bff72a
Pablo <pabloneny@gmail.com>
2018-02-12 14:40:13 -0500

    bare canvas with script and prefab

---
+++ Assets/Scripts/TonLivre.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TonLivre : MonoBehaviour
+{
+
+	void Start ()
+    {
+        Debug.Log("Besame, besame mucho, \n Como si fuera esta noche la última vez ");
+    }
+	
+	void Update ()
+    {
+        if (true)
+        {
+            
+        }
+	}
+}
