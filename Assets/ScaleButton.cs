ffa8f307c81f304dc9f9870ff5af5b043410b493
EveGaboury <eve.gaboury@gmail.com>
2018-02-16 18:07:04 -0500

    suite_Desktop_bouttondemierda

---
+++ Assets/ScaleButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleButton : MonoBehaviour {
+
+	public Renderer render;
+
+	void Start () 
+	{
+		render = GetComponent <Renderer> ();
+	}
+
+	void OnMouseOver()
+	{
+		Debug.Log ("the mouse is over" + render.name); 
+	}
+
+	void OnMouseExit ()
+	{
+		Debug.Log ("the mouse is no longer on" + render.name);
+
+	}
+}
