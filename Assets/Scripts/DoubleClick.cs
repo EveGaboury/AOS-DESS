e08bd7311ef55d9383787b056425d019ce6dd020
EveGaboury <eve.gaboury@gmail.com>
2018-02-16 00:26:10 -0500

    incorporer atlas suite et double click script

---
+++ Assets/Scripts/DoubleClick.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class DoubleClick : MonoBehaviour {
+
+
+	public Button folder;
+
+	private int counter;
+	public float clickTimer = .5f;
+
+	public GameObject window; 
+
+
+	void Start () 
+	{
+		folder.onClick.AddListener (buttonListener);
+
+	}
+
+	private void buttonListener ()
+	{
+		counter++;
+		if (counter == 1) 
+		{
+			StartCoroutine ("doubleClickEvent");
+		}
+	}
+
+	IEnumerator doubleClickEvent ()
+	{
+		yield return new WaitForSeconds (clickTimer);
+		if (counter > 1) 
+		{
+			Debug.Log ("double click");
+			window.SetActive (true);
+		}
+
+		yield return new WaitForSeconds (.05f);
+		counter = 0;
+
+	}
+
+}
