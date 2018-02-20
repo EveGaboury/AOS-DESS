2229ef017e2eb8c0eb219eeb67726ce10cadebb5
Pablo <pabloneny@gmail.com>
2018-02-20 17:59:50 -0500

    v1 du prefab de texte surlignable

--- Assets/Scripts/InputFieldTest.cs
+++ Assets/Scripts/InputFieldTest.cs
@@ -3,31 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.EventSystems;
 
-public class InputFieldTest
+public class InputFieldTest : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private static InputFieldTest instance;
+    //Public
+    public Text selectable;
+    public bool isOver = false;
 
-    private InputFieldTest() { }
+    //Privée
+    Color startingColor = Color.black, highlighted = Color.red;
 
-    public int HP;
-
-    public static InputFieldTest Instance
+    void Start()
     {
-        get
-        {
-            if (instance == null)
-            {
-                instance = new InputFieldTest();
-            }
-            return instance;
-        } 
+        selectable.GetComponent<Outline>().enabled = false;
+        selectable.GetComponent<Shadow>().enabled = false;
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse enter");
+        isOver = true;
+
+        selectable.GetComponent<Outline>().enabled = isOver;
+        selectable.color = highlighted;
+        selectable.GetComponent<Shadow>().enabled = isOver;
+
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse exit");
+        isOver = false;
+
+        selectable.GetComponent<Outline>().enabled = isOver;
+        selectable.color = startingColor;
+        selectable.GetComponent<Shadow>().enabled = isOver;
+
+        
+    }
 }
 
 
-//Source: https://stackoverflow.com/questions/41391708/how-to-detect-click-touch-events-on-ui-and-gameobjects
+//Source pour le pointer dans le UI: https://stackoverflow.com/questions/41391708/how-to-detect-click-touch-events-on-ui-and-gameobjects && https://gamedev.stackexchange.com/questions/108625/how-to-detect-mouse-over-for-ui-image-in-unity-5
 
 /*
  public class InputFieldTest : MonoBehaviour
@@ -58,4 +77,24 @@ void Awake()
 //        Debug.Log(GameManager.gaugePsycho);
 //    }
 //}  
+ */
+
+/*
+     private static InputFieldTest instance;
+
+private InputFieldTest() { }
+
+public int HP;
+
+public static InputFieldTest Instance
+{
+    get
+    {
+        if (instance == null)
+        {
+            instance = new InputFieldTest();
+        }
+        return instance;
+    } 
+}
  */
