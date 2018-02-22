51b09ff3d7529ab1ccbe9e4c1e9a7799a425f156
EveGaboury <eve.gaboury@gmail.com>
2018-02-22 17:39:11 -0500

    Recherche_inputfieldclear

---
+++ Assets/InputFieldReset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldReset : MonoBehaviour {
+
+	public Text inputFieldBrowser;
+	public string reset = " " ;
+	private bool clear = true;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (string.IsNullOrEmpty(" ")) {
+			clear = false;
+			Debug.Log ("ya de la merde écrite");
+		} 
+		
+
+	
+		
+	}
+}
