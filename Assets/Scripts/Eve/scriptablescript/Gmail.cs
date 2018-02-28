a9b83a1a514acbf588593edb0531df0735922ce1
EveGaboury <eve.gaboury@gmail.com>
2018-02-27 21:07:24 -0500

    starting scriptable

---
+++ Assets/Scripts/Eve/scriptablescript/Gmail.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GmailScriptable", menuName = "MessageScriptable")]
+public class Gmail : ScriptableObject {
+
+	public string name;
+	public string objet;
+	public TextMesh message;
+
+}
