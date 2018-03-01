61a9decf548a901cc63ccc050566cd0e1cd9ea95
EveGaboury <eve.gaboury@gmail.com>
2018-03-01 16:28:30 -0500

    Scene_Eve_Adding sprites for facebook

---
+++ Assets/Scripts/Eve/TextMeshPro/WhiteText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WhiteText : MonoBehaviour {
+
+	private TextMeshPro w_TextMeshPro;
+
+	// Use this for initialization
+	void Start () {
+
+		w_TextMeshPro = GetComponent<TextMeshPro> ();
+
+		w_TextMeshPro.color = Color.white;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
