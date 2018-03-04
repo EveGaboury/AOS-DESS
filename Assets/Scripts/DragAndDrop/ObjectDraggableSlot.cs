0a0f5e8697a2c012b3687174593473f2bb8ca58c
Pablo <pabloneny@gmail.com>
2018-03-04 15:50:58 -0500

    Prefab de photos draggables dans le Bloc Notes + Slider du bloc notes fonctionnel

--- Assets/Scripts/DragAndDrop/ObjectDraggableSlot.cs
+++ Assets/Scripts/DragAndDrop/ObjectDraggableSlot.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
+
 
 public class ObjectDraggableSlot : MonoBehaviour, IDropHandler
 {
 	public string[] newText;
 	public int[] checkID;
+
+	//[HideInInspector] public float totalHeight;
+
+	List<Transform> list = new List<Transform>();
 
 	public void OnDrop (PointerEventData eventData)
 	{
@@ -21,9 +27,38 @@ public class ObjectDraggableSlot : MonoBehaviour, IDropHandler
 				if (tri.objectID == checkID [i]) 
 				{
 					tri.transform.SetParent(transform);
-					tri.GetComponent<HighlightableText> ().intialText = newText [i];
+					if (tri.GetComponent<Image> ()) 
+					{
+						Debug.Log ("Une image a été ajoutée au bloc notes");
+					} 
+					else 
+					{
+						tri.GetComponent<HighlightableText> ().intialText = newText [i];
+					}
 				}
 			}
 		}
+	}
+
+	void OnTransformChildrenChanged()
+	{
+//		foreach (Transform t in transform) 
+//		{
+//			list.Add (t.gameObject);
+//			//Debug.Log (list.Where(h => h.totalHeight == t.gameObject.GetComponent<RectTransform>().rect.height).Sum(h => h.ToString()));
+////			float sum = t.gameObject.GetComponent<RectTransform> ().rect.height;
+////			while (n != 0)
+////			{
+////				sum += n % 10;
+////				n /=10;
+////			}
+//
+//			//int result = list.FindAll (t.gameObject.GetComponent<RectTransform> ()).Sum (c => c - '0');
+//
+////			if (list.Sum(t.gameObject.GetComponent<RectTransform>().rect.height) >= transform.GetComponent<RectTransform>().rect.height) 
+////			{
+////				Debug.Log ("I will survive");
+////			}
+//		}
 	}
 }
