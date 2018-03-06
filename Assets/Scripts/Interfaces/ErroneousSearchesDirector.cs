0bcac417da59b35998f63dc9d3a34bbbaccdf132
Pablo <pabloneny@gmail.com>
2018-03-06 15:03:21 -0500

    v2 faux resulats dans fb

--- Assets/Scripts/Interfaces/ErroneousSearchesDirector.cs
+++ Assets/Scripts/Interfaces/ErroneousSearchesDirector.cs
@@ -6,19 +6,49 @@ using System;
 
 public class ErroneousSearchesDirector : MonoBehaviour 
 {
+	public GameObject wrongSearchResults;
+
+	TextMeshProUGUI displayText;
+
+	int[] minuscules = new int[26], majuscules = new int[26];
+
+//	char[] majusculesss = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+//	char[] minusculessss = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		
+
 	char[] verification = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 						   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	
+	void Awake()
+	{
+		displayText = wrongSearchResults.GetComponentInChildren<TextMeshProUGUI>();
+
+		displayText.text = "";
+	}
 
 	public void Hello_World (string userSearch) 
 	{
-//		userSearch.Contains (verification);
+		char firstLetter = userSearch[0];
 
-		for (int i = 0; i < verification.Length; i++) 
+		if (firstLetter < verification[25] && firstLetter > verification[0]) 
 		{
-			if (userSearch[0] == verification[i]) 
-			{
-				Debug.Log ("The greatest success of: " + verification[i]);
-			}
+			Debug.Log ("VICTORY: " + userSearch);
+			wrongSearchResults.SetActive (true);
 		}
+		else if(firstLetter < verification[51] && firstLetter > verification[26])
+		{
+			Debug.Log ("VICTORY: " + userSearch);
+		}
+		else
+		{
+			Debug.Log ("The greatest FAILURE of: " + userSearch);
+		}
+	}
+
+	void OnDisable()
+	{
+		Debug.Log ("This was a triumph! \n I'm making a note here: \n Huge success!");
+		wrongSearchResults.SetActive (false);
+		this.gameObject.GetComponent<TMP_InputField> ().text = "";
 	}
 }
