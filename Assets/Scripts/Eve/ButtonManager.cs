59ef87cfe26eb4a4effaad172839af69757b7ce9
EveGaboury <eve.gaboury@gmail.com>
2018-03-05 20:47:53 -0500

    Scene_Eve_update mot de passe oublie

--- Assets/Scripts/Eve/ButtonManager.cs
+++ Assets/Scripts/Eve/ButtonManager.cs
@@ -8,6 +8,8 @@ using TMPro;
 public class ButtonManager: MonoBehaviour {
 
 
+	public StartPosition SP;
+
 	//game state
 	public GameObject sessionCass;
 	public bool SoOpen = true;
@@ -30,11 +32,15 @@ public class ButtonManager: MonoBehaviour {
 	//inputfield et boutton pour facebook mot de passe
 	public Button inputfieldWrongButton;
 	public TMP_InputField inputfieldWrongText;
-	private string reponsecorrecte = "cassandraroyer@gmail.com" ; 
-
-	public StartPosition SP;
+	private string reponsecorrecte = "cassandraroyer@courriel.fr", reponsecorrecte2 = "Cassandraroyer@courriel.fr" ; 
 
 	public GameObject faux;
+	public Image bouttonWrongPass;
+
+	public Sprite vrai;
+	public Sprite pasvrai;
+	public TMP_InputField inputfieldQuestion1;
+
 
 
 
@@ -49,47 +55,60 @@ public class ButtonManager: MonoBehaviour {
 
 		Button btnn = inputfieldWrongButton.GetComponent<Button> ();
 		btnn.onClick.AddListener (TaskOnClickForgotFacebook);
+
+
+		SpriteRenderer initial = GetComponent<SpriteRenderer> ();
+
+		TextMeshPro inputfieldQuestion1 = GetComponent <TextMeshPro> ();
+
+	
 	}
 
-	void Update () {
 
 
+	void Update () 
+	{
 		//game state
 		if (sessionCass.activeSelf == true) {
 			SoOpen = false;
 		}
-		else
-		{
+		else{
 			SoOpen = true;
 		}
 	}
 
-	void TaskOnClickTrash () {
-
-		if (SoOpen) {
+	void TaskOnClickTrash () 
+	{
+		if (SoOpen) 
+		{
 			windowSo.SetActive (true);
 		} else {
 			windowCass.SetActive (true);
 		}
 	}
 
-	void TaskonClickBoutonRouge () {
-
+	void TaskonClickBoutonRouge () 
+	{
 		facebookHeader.SetActive (false);
 		browserCanvas.SetActive (false);
 		facebookCanvas.SetActive (false);
 	}
-
+		
 	 void TaskOnClickForgotFacebook ()
 	{
-		if (reponsecorrecte == inputfieldWrongText.text) {
+		if ((reponsecorrecte == inputfieldWrongText.text) || (reponsecorrecte2 == inputfieldWrongText.text)) {
 			SP.questionOne.SetActive (true);
-		} else
+			bouttonWrongPass.sprite = vrai;
+		} else {
 			faux.SetActive (true);
-			
-
-
-
+			bouttonWrongPass.sprite = pasvrai;
+		}
 	}
 
+	public void ClearContent (){
+
+		inputfieldWrongText.text = "";
+		inputfieldQuestion1.text = "";
+
+		}
 }
---
+++ Assets/Scripts/Eve/OnDisableScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class OnDisableScript : MonoBehaviour {
+
+
+	public ButtonManager BM;
+
+	void OnDisable()
+	{
+		Debug.Log ("cllleaaarrr");
+		BM.ClearContent ();
+	}
+}
