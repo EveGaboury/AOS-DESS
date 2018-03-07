95dc0bdd44b4a94ef2a3cf77b3b10ad27bbfad12
Pablo <pabloneny@gmail.com>
2018-03-07 15:28:12 -0500

    v2 iTunes

--- Assets/Scripts/Interfaces/ChangeMusic.cs
+++ Assets/Scripts/Interfaces/ChangeMusic.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
 
 [RequireComponent(typeof(AudioSource))]
 public class ChangeMusic : MonoBehaviour
 {	
+	public Button activatingButton;
 	public AudioClip[] clipList;
+
+	public GameObject testingThis;
+	public Transform testingLocation;
 
 	AudioSource audioSource;
 	int currentAudioIndex = 0;
 	bool isAudioPlaying;
 
-	void Start()
+	void Awake()
 	{
 		isAudioPlaying = true;
 
@@ -25,7 +32,30 @@ public class ChangeMusic : MonoBehaviour
 		audioSource.Play ();
 	}
 
-	void PlayMusicAtIndex(int k)
+	void Start() 
+	{
+		TextMeshProUGUI displayText = gameObject.GetComponentInChildren (typeof(TextMeshProUGUI)) as TextMeshProUGUI;
+
+		if (displayText != null)
+		{
+			displayText.text = "";
+			GameObject test = Instantiate(testingThis, testingLocation.localPosition, Quaternion.identity) as GameObject;
+			for (int i = 0; i < clipList.Length; i++) 
+			{
+				//displayText.text = clipList [0].name + "\r\n" + clipList [1].name ;
+
+				Debug.Log (clipList [i].name);
+			}
+
+			Debug.Log (displayText.gameObject.name);
+		} 
+		else
+		{
+			return;
+		}
+	}
+
+	public void PlayMusicAtIndex(int k)
 	{
 		if (k >= clipList.Length && k < 0) 
 		{
@@ -37,13 +67,13 @@ public class ChangeMusic : MonoBehaviour
 		audioSource.Play ();
 	}
 
-	void PlayNextMusic()
+	public void PlayNextMusic()
 	{
 		int k = (currentAudioIndex + 1) % clipList.Length;
 		PlayMusicAtIndex(k);
 	}
 
-	void PlayPreviousMusic()
+	public void PlayPreviousMusic()
 	{
 		int k = (currentAudioIndex - 1) % clipList.Length;
 
@@ -56,11 +86,25 @@ public class ChangeMusic : MonoBehaviour
 	}
 		
 	void Update()
-	{		
+	{	
 		if (audioSource.time == audioSource.clip.length) 
 		{
 			Debug.Log (audioSource.clip.name + " a terminer de jouer");
 			PlayNextMusic ();
+		}
+	}
+
+	public void ActivateAndDeactivateMusicInterface()
+	{
+		if (this.gameObject.activeSelf) 
+		{
+			this.gameObject.SetActive (false);
+			Debug.Log (gameObject.name + " is active");
+		} 
+		else 
+		{
+			this.gameObject.SetActive (true);
+			Debug.Log (gameObject.name + " is inactive");
 		}
 	}
 }
