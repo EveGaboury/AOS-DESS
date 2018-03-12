af8ac5a863bfe8b6c5982354e9d7a37f3ad54747
EveGaboury <eve.gaboury@gmail.com>
2018-03-12 06:37:42 -0400

    Scene_Eve End Jour 1

---
+++ Assets/FinalPost.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class FinalPost : MonoBehaviour {
+
+	public TMP_InputField SearchField;
+	public TextMeshProUGUI inputText;
+
+	public Button postButton;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		TextMeshPro searchField = GetComponent<TextMeshPro>();
+		
+	}
+
+	public void SearchPost(string userSearch)
+	{
+		if (SearchField.text == "www.avis-deces-cassandra-royer.fr") {
+			postButton.GetComponent<Button> ().enabled = true;
+		}
+	}
+}
