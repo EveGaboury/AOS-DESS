c9f24e44c3cbfb4b506e384d6d7d7322231ae806
Pablo <pabloneny@gmail.com>
2018-03-18 19:14:27 -0400

    petits bugs et corrections orthographiques

--- Assets/Scripts/Betisier/Test.cs
+++ Assets/Scripts/Betisier/Test.cs
@@ -8,37 +8,5 @@ using UnityEngine.Audio;
 
 public class Test : MonoBehaviour
 {
-	public List<GameObject> testingListed = new List<GameObject>();
 
-	void Start()
-	{
-		Component[] broDog;
-
-		broDog = GetComponentsInChildren (typeof(Transform/*Button*/));
-
-		if (broDog != null)
-		{
-			foreach (Transform/*Button*/ item in broDog) 
-			{
-				testingListed.Add (item.gameObject);
-
-				for (int i = 0; i < testingListed.Count; i++) 
-				{
-					if (testingListed[i].GetComponent<Button>() == true) 
-					{
-
-						Debug.Log (testingListed[i].name);
-					}
-
-					if (testingListed[i].gameObject.GetComponentInChildren<Button>() == true) 
-					{
-						Debug.Log (testingListed[i].name);
-					}
-//					Debug.Log (testingListed.Count);
-
-//					Debug.Log ("Le boutton: " + testingListed[i].name + ", dont le parent est: " + testingListed[i].gameObject.GetComponentInParent<Transform>().transform.parent.name);
-				}
-			}
-		}
-	}
 } 
---
+++ Assets/Scripts/DebugManager/ButtonSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour 
+{
+	public List<GameObject> childObjects = new List<GameObject> ();
+
+	void Start()
+	{
+		SearchAllButtonsInTheHierachy ();
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void SearchAllButtonsInTheHierachy()
+	{
+		Transform[] allChildren = GetComponentsInChildren<Transform> (true);
+
+		foreach (Transform child in allChildren) 
+		{
+			if (child.gameObject.tag == "CeciEstUnBouton")
+			{
+				childObjects.Add (child.gameObject);
+				Debug.Log (child.name);
+			}
+		}
+	}
+}
