e151952f3cfa62eabc8d7fe52511bff840231351
EveGaboury <eve.gaboury@gmail.com>
2018-03-26 17:03:52 -0400

    Scene_Eve fixing scaling problem for bloc note dropzone

--- Assets/Scripts/Interfaces/NewData.cs
+++ Assets/Scripts/Interfaces/NewData.cs
@@ -64,7 +64,7 @@ public class NewData : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
 		resultatBlocNotes.GetComponent<Transform> ().SetParent (parentToBe.transform);
 
-		resultatBlocNotes.GetComponent<Transform> ().localScale = new Vector2(1.0f, 1.0f);
+		//resultatBlocNotes.GetComponent<Transform> ().localScale = new Vector2(1.0f, 1.0f);
 
 		this.gameObject.GetComponent<Button> ().enabled = false;
 
