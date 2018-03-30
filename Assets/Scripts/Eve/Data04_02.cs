6f315a7ba4fa54f12704a3af47c7bb20a9b839ec
EveGaboury <eve.gaboury@gmail.com>
2018-03-30 01:52:11 -0400

    scene_Eve finished all data + script data pour 04-03

---
+++ Assets/Scripts/Eve/Data04_02.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data04_02 : MonoBehaviour {
+
+	public GameObject J04_03;
+
+
+	public GameObject parentToBe;
+
+	// Use this for initialization
+	void Start () {
+
+	parentToBe = GameObject.Find ("Content_InventorySlot");
+
+		StartCoroutine(wait());
+
+	}
+
+
+
+	 IEnumerator wait () {
+
+		yield return new WaitForSeconds (2.7f);
+		J04_03.SetActive (true);
+		J04_03.GetComponent<Transform> ().SetParent (parentToBe.transform);
+	}
+}
