adb6e344ea42610f3753776cde01e1de7f7ac719
EveGaboury <eve.gaboury@gmail.com>
2018-03-31 19:48:17 -0400

    Scene_Eve fix scroll problem _DONE

--- Assets/ForceReUpdate.cs
+++ Assets/ForceReUpdate.cs
@@ -5,8 +5,23 @@ using UnityEngine.UI;
 
 public class ForceReUpdate : MonoBehaviour {
 
+	public scrollRectPosition SR;
+
 	void OnEnable() {
 
+
+
+		StartCoroutine(wait());
 		Canvas.ForceUpdateCanvases();
+
+	}
+
+
+	IEnumerator wait () {
+		yield return new WaitForSeconds (0.001f);
+		SR.scrollFacebook.verticalNormalizedPosition = 0f;	
+
+
+
 	}
 }
