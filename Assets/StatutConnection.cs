a6d4eef9b4c03dca0aa69038d025bfb1d61c3961
EveGaboury <eve.gaboury@gmail.com>
2018-04-05 12:54:55 -0400

    Scene_EVE
    preparing script for dialog box in messenger

---
+++ Assets/StatutConnection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class StatutConnection : MonoBehaviour {
+
+
+	public Image statutConnection;
+	public Button marieEve;
+
+	public GameObject dialogBox;
+
+	public Transform viewPort;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+}
