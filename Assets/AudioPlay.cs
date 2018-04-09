adfcf82fee0b3b9b75bd29f067411fde6b0e89d0
EveGaboury <eve.gaboury@gmail.com>
2018-04-09 00:35:41 -0400

    Scene_Eve Added animation sprite sheet and pop up animation with sound

---
+++ Assets/AudioPlay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour {
+
+	public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+
+		AudioSource audio = GetComponent<AudioSource> ();
+		audio.Play ();
+		
+	}
+}
