a24a05963e5627a740e6fc7ecec680e17f271b51
Pablo <pabloneny@gmail.com>
2018-04-22 15:34:47 -0400

    reglage plusieurs bugs rapport au curseur

--- Assets/Scripts/Interfaces/AudioSourceManagerScript.cs
+++ Assets/Scripts/Interfaces/AudioSourceManagerScript.cs
@@ -6,6 +6,8 @@ public class AudioSourceManagerScript : MonoBehaviour
 {
 	public AudioSource audioSourceData, audioSourceBoutons, audioSourceClicksEtTyping, audioSourceCueEmotion, audioSourceMusique;
 
+	AudioSource[] localAudioSources;
+
 	void Awake()
 	{
 		DetectAudioSources ();
@@ -13,7 +15,7 @@ public class AudioSourceManagerScript : MonoBehaviour
 
 	public void DetectAudioSources()
 	{
-		AudioSource[] localAudioSources = this.gameObject.GetComponents<AudioSource> ();
+		localAudioSources = this.gameObject.GetComponents<AudioSource> ();
 
 		audioSourceData = localAudioSources [0];
 
@@ -25,4 +27,62 @@ public class AudioSourceManagerScript : MonoBehaviour
 
 		audioSourceMusique = localAudioSources [4];
 	}
+
+//	void Update()
+//	{
+//		if (Input.GetKey(KeyCode.A)) 
+//		{
+//			//audioSourceData.GetComponent<AudioSource>().enabled = false;
+//			//localAudioSources [0].enabled = false;
+//			//Debug.Log ("The Alpha1 key has been pressed.");
+//		}
+////		else if (Input.GetKeyUp(KeyCode.Alpha1))
+////		{
+////			localAudioSources [0].enabled = true;
+////		}
+//
+//
+//		if (Input.GetKey(KeyCode.Z)) 
+//		{
+//			localAudioSources [1].enabled = false;
+//			//Debug.Log ("The Alpha2 key has been pressed.");
+//		}
+////		else if (Input.GetKeyUp(KeyCode.Alpha2))
+////		{
+////			localAudioSources [1].enabled = true;
+////		}
+//
+//
+//		if (Input.GetKey(KeyCode.E)) 
+//		{
+//			localAudioSources [2].enabled = false;
+//			//Debug.Log ("The Alpha3 key has been pressed.");
+//		}
+////		else if (Input.GetKeyUp(KeyCode.Alpha3))
+////		{
+////			localAudioSources [2].enabled = true;
+////		}
+//
+//
+//		if (Input.GetKey(KeyCode.R)) 
+//		{
+//			localAudioSources [3].enabled = false;
+//			//Debug.Log ("The Alpha4 key has been pressed.");
+//		}
+////		else if (Input.GetKeyUp(KeyCode.Alpha4))
+////		{
+////			localAudioSources [3].enabled = true;
+////		}
+//
+//
+//		if (Input.GetKey(KeyCode.T)) 
+//		{
+//			localAudioSources [4].enabled = false;
+//			//Debug.Log ("The Alpha5 key has been pressed.");
+//		}
+////		else if (Input.GetKeyUp(KeyCode.Alpha5))
+////		{
+////			localAudioSources [4].enabled = true;
+////		}
+//	}
 }
