8a0ff74f324811e2cef80f243a716c21264ee006
EveGaboury <eve.gaboury@gmail.com>
2018-04-27 13:05:08 -0400

    Last_update

--- Assets/Scripts/Eve/GameState.cs
+++ Assets/Scripts/Eve/GameState.cs
@@ -358,14 +358,14 @@ public class GameState : MonoBehaviour {
 			FR.OnEnable ();
 
 			//cue emotion ici "final" il faut que ça le fasse une seule fois
-			if (playCueOnce == false) 
-			{
-				playCueOnce = true;
-
-				soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.clip = cue_emotion;
-
-				StartCoroutine(PlayAudio(cue_emotion));
-			}
+//			if (playCueOnce == false) 
+//			{
+//				playCueOnce = true;
+//
+//				soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.clip = cue_emotion;
+//
+//				StartCoroutine(PlayAudio(cue_emotion));
+//			}
 		}
 
 			DM.boulesale = true;
@@ -1022,46 +1022,46 @@ public class GameState : MonoBehaviour {
 
 	}
 
-	IEnumerator PlayAudio(AudioClip tuneToPlay)
-	{
-		soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.PlayOneShot (tuneToPlay);
-
-		StartCoroutine(CrossFadeBetweenTunes(soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceMusique, soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion, 1.0f));
-
-		float localFloat = tuneToPlay.length;
-
-		yield return new WaitForSeconds (localFloat);
-
-		soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.clip = null;
-	}
-
-	IEnumerator CrossFadeBetweenTunes (AudioSource a, AudioSource b, float seconds)
-	{
-		float step_interval = seconds / 1.0f;
-
-		float volume_interval = MusicVolume / 5.0f;
-
-		for (int i = 0; i < 20; i++) 
-		{
-			a.volume -= volume_interval;
-			b.volume += volume_interval;
-
-			yield return new WaitForSeconds (step_interval);
-		}
-	}
-
-	void ResetVolumeValues()
-	{
-		StartCoroutine(CrossFadeBetweenTunes(soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion, soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceMusique,1.0f));
-	}
-
-	void FixedUpdate()
-	{
-		if (soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.clip == cue_emotion) 
-		{
-			Invoke ("ResetVolumeValues", soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.clip.length);
-
-			//Debug.Log ("la cue d'emotion " + cue_emotion + " est entrain de jouer.");
-		}
-	}
+//	IEnumerator PlayAudio(AudioClip tuneToPlay)
+//	{
+//		soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.PlayOneShot (tuneToPlay);
+//
+//		StartCoroutine(CrossFadeBetweenTunes(soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceMusique, soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion, 1.0f));
+//
+//		float localFloat = tuneToPlay.length;
+//
+//		yield return new WaitForSeconds (localFloat);
+//
+//		soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.clip = null;
+//	}
+//
+//	IEnumerator CrossFadeBetweenTunes (AudioSource a, AudioSource b, float seconds)
+//	{
+//		float step_interval = seconds / 1.0f;
+//
+//		float volume_interval = MusicVolume / 5.0f;
+//
+//		for (int i = 0; i < 20; i++) 
+//		{
+//			a.volume -= volume_interval;
+//			b.volume += volume_interval;
+//
+//			yield return new WaitForSeconds (step_interval);
+//		}
+//	}
+//
+//	void ResetVolumeValues()
+//	{
+//		StartCoroutine(CrossFadeBetweenTunes(soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion, soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceMusique,1.0f));
+//	}
+//
+//	void FixedUpdate()
+//	{
+//		if (soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.clip == cue_emotion) 
+//		{
+//			Invoke ("ResetVolumeValues", soundManager.GetComponent<AudioSourceManagerScript> ().audioSourceCueEmotion.clip.length);
+//
+//			//Debug.Log ("la cue d'emotion " + cue_emotion + " est entrain de jouer.");
+//		}
+//	}
 }
